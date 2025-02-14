d8c025567da734cbea91618ce3b30a5c62598ad6
KuanJu <ab82825519@gmail.com>
2025-02-14 14:24:50 +0800

    修改會員管理員重複新增邏輯，修改為create模式

--- projfinal-back/src/main/java/tw/com/ispan/repository/admin/MemberRepository.java
+++ projfinal-back/src/main/java/tw/com/ispan/repository/admin/MemberRepository.java
@@ -11,7 +11,14 @@ import org.springframework.data.repository.query.Param;
 import tw.com.ispan.domain.admin.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
-
+	
+	//初始化資料避免一直重複(冠)
+	//由於僅提供內部資料初始化使用，因此不怕SQL injection
+	@Query("SELECT m.id FROM Member m WHERE m.id BETWEEN :start AND :end")
+	List<Integer> findMemberIdsInRange(@Param("start") int start, @Param("end") int end);
+	
+	
+	
 	//(冠)
 	Optional<Member> findByNickName(String nickName);
 	//(冠)
